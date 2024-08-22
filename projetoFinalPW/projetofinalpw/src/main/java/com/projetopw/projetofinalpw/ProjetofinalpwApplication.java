8ae5ac6989bf4499cb09ab53149b500f6256c568
Andre Luiz <andre2kluiz@gmail.com>
2024-08-22 09:05:36 -0300

    primeiro commit

---
+++ projetoFinalPW/projetofinalpw/src/main/java/com/projetopw/projetofinalpw/ProjetofinalpwApplication.java
@@ -0,0 +1,13 @@
+package com.projetopw.projetofinalpw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetofinalpwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetofinalpwApplication.class, args);
+	}
+
+}
---
+++ projetoFinalPW/projetofinalpw/src/test/java/com/projetopw/projetofinalpw/ProjetofinalpwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetopw.projetofinalpw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetofinalpwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
